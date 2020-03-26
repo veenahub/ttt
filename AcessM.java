00083e8b53c4bfe3bd23a3ef2789614f64a7ea40
veenahub <veena.pattigulla@gmail.com>
2020-03-26 13:37:42 +0100

    different process

---
+++ AcessM.java
@@ -0,0 +1,16 @@
+package project1;
+
+public class AcessM {
+public static void main(String[]args) {
+	//AcessM AM = new AcessM();
+    //  AM.m1();
+      InstantV  v1= new InstantV(15);
+      System.out.println(v1.getX());
+      v1.withdraw(10);
+      System.out.println(v1.getX());
+      v1.withdraw(10);
+      System.out.println(v1.getX());
+	//AM.x = 100;
+
+}
+}
